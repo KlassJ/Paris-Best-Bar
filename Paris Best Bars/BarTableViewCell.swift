f28cf6b6753a770b2f36d9d944978f8ac1a0638b
Vilayvone Bouahom <vivi@vilayvones-macbook-air.local>
2016-04-18 16:03:28 +0200

    first Commit

---
+++ Paris Best Bars/BarTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BarTableViewCell.swift
+//  Paris Best Bars
+//
+//  Created by Vilayvone Bouahom on 18/04/16.
+//  Copyright © 2016 Jbouahom. All rights reserved.
+//
+
+import UIKit
+
+class BarTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
